faafb836f1ad26b17e3ece8aeb607be34fa86546
Gerard O'Neill <grardb@gmail.com>
2018-09-14 18:45:32 -0700

    Customize 404 error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception->getStatusCode() === 404) {
+            return response()->json(['error' => 'API endpoint not found'], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
