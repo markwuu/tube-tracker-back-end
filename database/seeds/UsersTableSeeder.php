b96e9bfb354304c1dd7dc20662e2b175321e3f28
Gerard O'Neill <grardb@gmail.com>
2018-09-16 13:46:24 -0700

    Fix User Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder {
     public function run() {
-        factory(App\User::class, 10)->create();
+        factory(App\Models\User::class, 10)->create();
     }
 }
